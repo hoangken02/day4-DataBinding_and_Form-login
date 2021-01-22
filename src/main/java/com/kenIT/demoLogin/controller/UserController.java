248bd88a264ef9ab0772a6782fa8ce6433624830
- <=>
2021-01-22 10:51:52 +0700

    day4

---
+++ src/main/java/com/kenIT/demoLogin/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.kenIT.demoLogin.controller;
+
+import com.kenIT.demoLogin.dao.UserDao;
+import com.kenIT.demoLogin.model.Login;
+import com.kenIT.demoLogin.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/home")
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView("home","login",new Login());
+        return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login){
+        User user = UserDao.checkLogin(login);
+        ModelAndView modelAndView;
+        if (user == null){
+            modelAndView = new ModelAndView("error");
+        }else {
+            modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user",user);
+        }
+        return modelAndView;
+    }
+}
